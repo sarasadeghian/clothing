e1bea558f31211b47cfce360e95d03a36cbfce90
Sara Sadeghian <sara.sadeghian.t@gmail.com>
2020-10-16 16:15:02 +0330

    adding routing to switch between home page and sample hatspage

--- clothing/src/App.tsx
+++ clothing/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+
+const HatsPage = () => ( <div>
+  <h1>Hats Page</h1>
+</div>)
 
 function App() {
   return (
-    <div>
-      <HomePage/>
-    </div>
+    <Switch>
+      <Route exact path='/'>
+        <HomePage/>
+      </Route>
+      <Route exact path='/hats'>
+        <HatsPage/>
+      </Route>
+    </Switch>
   );
 }
 
